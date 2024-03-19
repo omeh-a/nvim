a670d933c83e5a2a012722d1b81b73dd9f3d7b85
Matt Rossouw <39023043+omeh-a@users.noreply.github.com>
2024-03-19 16:56:55 +1100

    Added python LSP server to config

--- init.lua
+++ init.lua
@@ -627,6 +627,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        pylsp = {},
 
         lua_ls = {
           -- cmd = {...},
