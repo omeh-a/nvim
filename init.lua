4ad6a935cde183c3a2d6a73bff9e6604b9820942
Matt Rossouw <39023043+omeh-a@users.noreply.github.com>
2024-11-29 13:04:39 +1100

    Rebase on new template; addded telescope shortcut

--- init.lua
+++ init.lua
@@ -411,6 +411,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
